45911db112f22fbf00f904be07ab7aec2c40a5cf
XDancer <111144908+andylebeau@users.noreply.github.com>
2023-07-22 14:28:16 -0400

    Step 2 of TOP Assignment

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 const Gameboard = (() => {
-    let board = ['', '', '', '', '', '', '', '', ''];
+    let gameboard = ['', '', '', '', '', '', '', '', ''];
 
+    const render = () => {
+        let gameboardHTML = '';
+        gameboard.forEach((cellValue, index) => {
+            gameboardHTML += `<div class="cell" id="${index}">${cellValue}</div>`
+        })
+        document.querySelector('#gameBoard').innerHTML = gameboardHTML;
+    }
+    return {
+        render
+    }
+})();
+
+const players = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+const startBtn = document.getElementById('startBtn');
+startBtn.addEventListener('click', () => {
+    Gameboard.render();
+})
+
+const GameFlow = (() => {
+    //check player
+    //check player mark
+    //check player selection
+    //check for win and display message
+    //render
 })();
