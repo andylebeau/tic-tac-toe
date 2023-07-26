d4d3a148d90fe45fee1b97c024cb3ebccbca3851
XDancer <111144908+andylebeau@users.noreply.github.com>
2023-07-26 16:57:25 -0400

    Step 4 fo TOP Assignment. Working on 4.1

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
 const Gameboard = (() => {
-    let gameboard = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+  let _board = Array.from({length: 9}).fill('')
+  let _gameboardHTML = '';
+  let checkWinArray = _board.slice()
 
-    const render = () => {
-        let gameboardHTML = '';
-        gameboard.forEach((cellValue, index) => {
-            gameboardHTML += `<div class="cell" id="${index}">${cellValue}</div>`
-        })
-        document.querySelector('#gameBoard').innerHTML = gameboardHTML;
-    }
-    return {
-        render
-    }
+  const createBoard = () => {
+    _board.forEach((cellValue, index) => {
+      _gameboardHTML += `<div class="cell" data-index="${index}">${cellValue}</div>`
+    })
+    document.querySelector('#gameBoard').innerHTML = _gameboardHTML;
+  }
+  return {
+      createBoard,
+      checkWinArray
+  }
 })();
 
-Gameboard.render() //temp auto start
+// function checkWin(cellIndex, checkArray) {
+//   checkArray.push(cellIndex)
+//   console.log(checkArray)
+// }
 
-const startGame = (() => {
-    const players = [
-        createPlayer(document.getElementById('player1').value, 'X'),
-        createPlayer(document.getElementById('player2').value, 'O'),
-    ]
-    Gameboard.render();
-})
+function gameFlow(mark) {
+  function updateBoard(event) {
+    if (event.target.textContent != '') {return}
+    event.target.textContent = `${mark}`
+    console.log(event.target.dataset.index)
+    Gameboard.checkWinArray[event.target.dataset.index] = mark;
+    console.log(Gameboard.checkWinArray)
+    mark = ['X', 'O'].filter(m => mark != m)[0]
+  }
+  
+  const cellClick = document.querySelectorAll('.cell')
+  cellClick.forEach((c) => c.addEventListener('click', updateBoard))
+};
+
+const startGame = () => {
+  let mark = 'X'
+  Gameboard.createBoard()
+  gameFlow(mark)
+}
 
 const startBtn = document.getElementById('startBtn');
 startBtn.addEventListener('click', startGame);
-
-const createPlayer = (name, mark) => {
-    return {
-        name,
-        mark
-    }
-}
-
-const GameFlow = (() => {
-    //check player
-    //check player mark
-    //check player selection
-    //check for win and display message
-    //render
-})();
